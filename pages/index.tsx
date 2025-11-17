06270b21aafa415d2200d446c75bf85f121b3131
Fadhal Shulhan <fadhalshulhan@gmail.com>
2025-11-17 15:37:41 +0700

    Add Head component to IndexPage for SEO and favicon support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from 'next/head';
 import BoothSelector from '@/components/BoothSelector';
 
 export default function IndexPage() {
-  return <BoothSelector />;
+  return (
+    <>
+      <Head>
+        <title>Experience Booth Cekat Innova</title>
+        <link rel="icon" href="/logos/cekat.png" />
+      </Head>
+      <BoothSelector />
+    </>
+  );
 }
